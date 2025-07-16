85b97f50fc7d6cc27b49a28814d74bc7e700db50
Adrian Bunn <masterhellish@gmail.com>
2025-07-16 15:42:29 +0100

    Gave BASE_DIR a comment for server scripts

--- Scripts/StartOpenTTDServer.bat
+++ Scripts/StartOpenTTDServer.bat
@@ -4,8 +4,10 @@ REM Launches OpenTTD in debug mode from the base folder.
 REM Created by Master Hellish – https://masterhellish.net
 REM Feel free to copy, change, or distribute as required.
 REM Use at your own risk – no warranty or guarantee provided.
+REM 
+REM Set BASE_DIR below to the folder where YOUR OpenTTD is installed.
 REM ============================================
 
-set "BASE_DIR=G:\OpenTTDViewerPlus"
+set "BASE_DIR=G:\OpenTTD"
 
 start /D "%BASE_DIR%" openttd.exe -D
--- Scripts/StartOpenTTDServerWithLastAutoSave.bat
+++ Scripts/StartOpenTTDServerWithLastAutoSave.bat
@@ -4,9 +4,11 @@ REM Launches OpenTTD with the most recent autosave.
 REM Created by Master Hellish – https://masterhellish.net
 REM Feel free to copy, change, or distribute as required.
 REM Use at your own risk – no warranty or guarantee provided.
+REM 
+REM Set BASE_DIR below to the folder where YOUR OpenTTD is installed.
 REM ============================================
 
-set "BASE_DIR=G:\OpenTTDViewerPlus"
+set "BASE_DIR=G:\OpenTTD"
 setlocal enabledelayedexpansion
 
 FOR /F "DELIMS=" %%F IN ('DIR /B /A-D /OD /TW "%BASE_DIR%\save\autosave\autosave*.sav"') DO (
